c3221b325501d3984f0323dd2c6291def2a9b3cf
Jared Dyreson <jared.dyreson@gmail.com>
2021-06-15 21:47:08 -0700

    doing a basic driver code test run, fingers crossed

--- UnitTests/test_tuffix_driver.py
+++ UnitTests/test_tuffix_driver.py
@@ -9,6 +9,7 @@ from Tuffix.Driver import *
 from Tuffix.Silencer import silence
 import unittest
 
+
 class DriverTest(unittest.TestCase):
      def test_init(self):
          """
@@ -17,112 +18,111 @@ class DriverTest(unittest.TestCase):
 
          command = "tuffix init"
          main(command.split())
-    def test_background(self):
-        # file that exists
-        command = "tuffix background /home/jared/Pictures/Wallpapers/linux.jpg"
-        try:
-            main(command.split())
-            # with silence(): main(command.split())
-        except FileNotFoundError:
-            self.assertTrue(False)
-        else:
-            self.assertTrue(True)
-
-        # file that does not exist
-        command = "tuffix background /tmp/phony_image.png"
-        try:
-            main(command.split())
-            # with silence(): main(command.split())
+    # def test_background(self):
+        # # file that exists
+        # command = "tuffix background /home/jared/Pictures/Wallpapers/linux.jpg"
+        # try:
             # main(command.split())
-        except FileNotFoundError:
-            self.assertTrue(True)
-        else:
-            self.assertTrue(False)
+            # # with silence(): main(command.split())
+        # except FileNotFoundError:
+            # self.assertTrue(False)
+        # else:
+            # self.assertTrue(True)
 
-        command = "tuffix background user-submitted"
-        # with silence(): main(command.split())
+        # # file that does not exist
+        # command = "tuffix background /tmp/phony_image.png"
+        # try:
+            # main(command.split())
+            # # with silence(): main(command.split())
+            # # main(command.split())
+        # except FileNotFoundError:
+            # self.assertTrue(True)
+        # else:
+            # self.assertTrue(False)
+
+        # command = "tuffix background user-submitted"
+        # # with silence(): main(command.split())
+        # main(command.split())
+
+        # command = "tuffix background"
+
+        # try:
+            # main(command.split())
+            # # with silence(): main(command.split())
+        # except UsageError:
+            # self.assertTrue(False)
+        # else:
+            # self.assertTrue(True)
+
+
+    def test_editor(self):
+        """
+        Add and remove an editor: emacs
+        """
+
+        command = "tuffix editor add emacs"
+        main(command.split())
+        command = "tuffix editor remove emacs"
         main(command.split())
 
-        command = "tuffix background"
+    def test_list(self):
+        """
+        List all viable commands
+        """
 
-        try:
+        command = "tuffix list"
+        main(command.split())
+
+    def test_describe(self):
+        """
+        Print all descriptions of keywords
+        """
+
+        k_container = KeywordContainer(DEFAULT_BUILD_CONFIG)
+        k_container.container.sort(key=lambda x: x.name)
+        for keyword in k_container.container:
+            command = f'tuffix describe {keyword.name}'
             main(command.split())
-            # with silence(): main(command.split())
-        except UsageError:
-            self.assertTrue(False)
-        else:
-            self.assertTrue(True)
 
+    def test_add(self):
+        """
+        Add keyword `test`
+        """
 
-    # def test_editor(self):
-        # """
-        # Add and remove an editor: emacs
-        # """
+        command = "tuffix add test"
+        main(command.split())
 
-        # command = "tuffix editor add emacs"
-        # main(command.split())
-        # command = "tuffix editor remove emacs"
-        # print(command)
-        # main(command.split())
+    def test_remove(self):
+        """
+        Remove keyword `test`
+        """
 
-    # def test_list(self):
-        # """
-        # List all viable commands
-        # """
+        command = "tuffix remove test"
+        main(command.split())
 
-        # command = "tuffix list"
-        # main(command.split())
+    def test_installed(self):
+        """
+        List all installed keywords
+        """
 
-    # def test_describe(self):
-        # """
-        # Print all descriptions of keywords
-        # """
+        command = "tuffix installed"
+        main(command.split())
 
-        # k_container = KeywordContainer(DEFAULT_BUILD_CONFIG)
-        # k_container.container.sort(key=lambda x: x.name)
-        # for keyword in k_container.container:
-            # command = f'tuffix describe {keyword.name}'
-            # main(command.split())
+    def test_status(self):
+        """
+        Print status command to the console
+        """
 
-    # def test_add(self):
-        # """
-        # Add keyword `test`
-        # """
+        command = "tuffix status"
+        main(command.split())
 
-        # command = "tuffix add test"
-        # main(command.split())
+    def test_custom(self):
+        """
+        Install custom keyword via JSON
+        """
 
-    # def test_remove(self):
-        # """
-        # Remove keyword `test`
-        # """
-
-        # command = "tuffix remove test"
-        # main(command.split())
-
-    # def test_installed(self):
-        # """
-        # List all installed keywords
-        # """
-
-        # command = "tuffix installed"
-        # main(command.split())
-
-    # def test_status(self):
-        # """
-        # Print status command to the console
-        # """
-
-        # command = "tuffix status"
-        # main(command.split())
-
-    # def test_custom(self):
-        # """
-        # Install custom keyword via JSON
-        # """
-
-        # command = "tuffix custom json_payload/OSC.json"
-        # main(command.split())
+        command = "tuffix custom json_payload/OSC.json"
+        main(command.split())
 
 
     # def test_sysupgrade(self):
