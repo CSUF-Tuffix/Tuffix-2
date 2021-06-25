1c4958786370df1ca7e24a25e0e24982ebe828a7
Jared Dyreson <jared.dyreson@gmail.com>
2021-06-24 17:47:33 -0700

    added RemoveCommand test

--- UnitTests/Commands/test_remove_command.py
+++ UnitTests/Commands/test_remove_command.py
@@ -1,4 +1,4 @@
-from Tuffix.Commands import AddCommand, AbstractCommand
+from Tuffix.Commands import RemoveCommand, AbstractCommand
 from Tuffix.Configuration import DEFAULT_BUILD_CONFIG
 
 import unittest
@@ -8,6 +8,6 @@ class AddCommandTest(unittest.TestCase):
     # NOTE: this test has been done in Commands/test_add_remove_helper.py
     def test_init(self):
         try:
-            AddCommand(DEFAULT_BUILD_CONFIG)
+            RemoveCommand(DEFAULT_BUILD_CONFIG)
         except ValueError:
             self.assertTrue(False)
