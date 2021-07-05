8bd9992b7a60d28c5b18dd5d66f51e1c21558905
Jared Dyreson <jared.dyreson@gmail.com>
2021-07-05 13:06:52 -0700

    Did some cleaning up in the base directory and the runner script

--- runner.py
+++ runner.py
@@ -14,47 +14,76 @@ runner = unittest.TextTestRunner()
 
 
 def construct_filesystem(pedantic):
+    """
+    Read the contents of `UnitTests` and attempt to construct
+    a working structure to load all valid tests
+
+    We can also specify which directories/files should be ignored
+    during construction.
+
+    pedantic: allows for the test to print to stdout/stderr. This can be toggled to
+        False to allow for less output.
+
+    """
+
     if not(isinstance(pedantic, bool)):
         raise ValueError(f'{pedantic=} is not a `bool`')
+
+    parent_dir = pathlib.Path("UnitTests")
 
     excluded_dirs = ["__pycache__", "TEST"]
     excluded_files = ["SequentialTest.py", "__init__.py", "BaseTester.py"]
     container = []
 
-    for dirpath, dirs, filepath in os.walk("UnitTests", topdown=True):
+    for dirpath, dirs, filepath in os.walk(parent_dir, topdown=True):
         dirs.sort()
         dirs[:] = [d for d in dirs if d not in excluded_dirs]
         filepath[:] = [f for f in filepath if f not in excluded_files]
         test_name = os.path.basename(dirpath)
-        if not(test_name == "UnitTests"):
+        if not(test_name == str(parent_dir)):
             filepath = [(path, pedantic) for path in filepath]
             container.append(
                 {test_name: filepath}
             )
     return container
-    # return sorted(container, reverse=False)
 
 
 def conduct_test(path: pathlib.Path, pedantic: bool):
+    """
+    Run the test given a path to the module and if it will be pendantic
+    """
+
     if not(isinstance(path, pathlib.Path) and
            isinstance(pedantic, bool)):
         raise ValueError(f'{path=} is not a `pathlib.Path`')
     test_re = re.compile(".*Test")
 
+    # get the specifications from the path given
     spec = importlib.util.spec_from_file_location("test", path)
+    # load the specifications as a module to be imported
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
+
     if(hasattr(module, 'IGNORE_ME')):
+        # to make testing go by quicker
+        # please remove `IGNORE_ME = True` from all instances
+
         print(f'[INFO] Ignoring {path}')
         return
+
+    # these tests names must conform to a certain format
+    # for example: ExampleTest
+    # not working example: SomethingOrAnother
+
     tests = [_test for _test in dir(module) if (test_re.match(_test))]
 
     for _test in tests:
         print(f'[INFO] Conducting {_test}')
+        # get the current instance of the class we are interested in
         test_instance = getattr(module, _test, None)
-        # if(hasattr(test_instance, 'setUp')):
-        # test_instance.setUp()
+        # load the current test into a testloader for unittest
         test_suite = SequentialTestLoader().loadTestsFromTestCase(test_instance)
+        # send all output from functions to /dev/null if specified
         if(pedantic):
             runner.run(test_suite)
         else:
@@ -63,6 +92,10 @@ def conduct_test(path: pathlib.Path, pedantic: bool):
 
 
 def run_tests():
+    """
+    Driver code for the two functions above
+    """
+
     tests = construct_filesystem(pedantic=True)
     base_folder = pathlib.Path("UnitTests")
     for test in tests:
